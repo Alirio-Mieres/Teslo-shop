8a7a776b5a4809d6a315b993a505fe49e5b70737
alirio <mie21001@byui.edu>
2022-11-05 19:29:59 -0400

    Documentar DTOs

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,15 +1,21 @@
-import { Type } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsPositive, Min } from "class-validator";
+import { Type } from "class-transformer";
 
 export class PaginationDto{
     
-
+    @ApiProperty({
+        default: 10, description: 'How many rows do you need?'        
+    })
     @IsOptional()
     @IsPositive()
     //Transformar
     @Type( () => Number ) // enableImplicitConversions: true
     limit?: number;
 
+    @ApiProperty({
+        default: 0, description: 'How many rows do you want to skip?'        
+    })  
     @IsOptional()
     @Min(0)
     @Type( () => Number ) // enableImplicitConversions: true
