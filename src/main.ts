d2dc08747cd0404bdc4957c283f32939ecf2dab2
alirio <mie21001@byui.edu>
2022-11-05 18:51:21 -0400

    nestjs swagger - OpenAPI Specification

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap'); 
+  const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
 
@@ -14,9 +17,19 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
     })
-   );
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('The Teslo Shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
 
   await app.listen(process.env.PORT);
-  logger.log(`App running on port ${ process.env.PORT }`);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
