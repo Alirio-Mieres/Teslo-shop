810910612936eca135a2e29f607cc26aea5ef8d2
alirio <mie21001@byui.edu>
2022-11-04 20:51:33 -0400

    Composición de Decoradores

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, SetMetadata } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { RawHeaders, GetUser } from './decorators';
-import { RoleProtected } from './decorators/role-protected.decorator';
+import { RawHeaders, GetUser, Auth, RoleProtected } from './decorators';
 import { LoginUserDto, CreateUserDto } from './dto';
 import { User } from './entities/user.entity';
 import { UserRoleGuard } from './guards/user-role.guard';
@@ -54,6 +53,15 @@ export class AuthController {
     }
   }
   
-
+  @Get('private3')
+  @Auth( ValidRoles.admin, ValidRoles.superUser )
+  privateRoute3(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user
+    }
+  }
 
 }
